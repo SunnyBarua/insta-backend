ff2c0c9da7a84e568023a89043849c4cd409f324
SunnyBarua <sunnybarua95@gmail.com>
2020-09-04 12:17:48 +0600

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors=require('cors')
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require("./keys");
+app.use(cors())
 
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
